ee06381be2276cddefaabbd6a957d4786c0c3d3c
Bawanthathilan <bawantharathnayaka@gmail.com>
2023-10-15 19:43:02 +0530

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Turborepo starter
+# NEXT JS Monorepo starter
 
 This is an official starter Turborepo.
 
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ export default function Page(): JSX.Element {
   return (
     <main className=" h-screen bg-gray-900">
       <div className="flex flex-row items-center justify-center h-full">
-        <h1 className="text-white">NEXT JS MONO REPO</h1>
+        <h1 className="text-white">NEXT JS MONO REPO / WEB APP</h1>
       </div>
     </main>
   );
